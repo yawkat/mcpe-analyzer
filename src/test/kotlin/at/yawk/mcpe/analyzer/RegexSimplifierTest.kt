6e81fec2956d2c174f00b0362104257e4da92fcf
yawkat <me@yawk.at>
2017-02-27 11:58:43 +0100

    Move regex unit tests to string representations

--- src/test/kotlin/at/yawk/mcpe/analyzer/RegexSimplifierTest.kt
+++ src/test/kotlin/at/yawk/mcpe/analyzer/RegexSimplifierTest.kt
@@ -11,68 +11,68 @@ class RegexSimplifierTest {
 
     @Test
     fun `simplify concat with repeat`() = Assert.assertEquals(
-            simplify(t(1) concat t(1).zeroOrMore() concat t(1)),
-            RegularExpression.Repeat(t(1), 2, null)
+            simplify(parseRegex("x x* x")),
+            parseRegex("x{2,}")
     )
 
     @Test
     fun `simplify simple union`() = Assert.assertEquals(
-            simplify(t(1) or t(2)),
-            t(1) or t(2)
+            simplify(parseRegex("x | y")),
+            parseRegex("x | y")
     )
 
     @Test
     fun `union epsilon absorption into zeroOrMore`() = Assert.assertEquals(
-            simplify(RegularExpression.empty<Int>() or t(0).zeroOrMore()),
-            t(0).zeroOrMore()
+            simplify(parseRegex(" | x*")),
+            parseRegex("x*")
     )
 
     @Test
     fun `union epsilon absorption into oneOrMore`() = Assert.assertEquals(
-            simplify(RegularExpression.empty<Int>() or (t(0).zeroOrMore() concat t(0))),
-            t(0).zeroOrMore()
+            simplify(parseRegex(" | x+")),
+            parseRegex("x*")
     )
 
     @Test
-    fun `nested zeroOrMore neutralize`() = Assert.assertEquals(
-            simplify(RegularExpression.Repeat(RegularExpression.Repeat(t(0), 0, null), 0, null)),
-            t(0).zeroOrMore()
+    fun `nested zeroOrMore neutralize`() =Assert.assertEquals(
+            simplify(parseRegex("x**")),
+            parseRegex("x*")
     )
 
     @Test
     fun `nested zeroOrMore + oneOrMore neutralize`() = Assert.assertEquals(
-            simplify(RegularExpression.Repeat(RegularExpression.Repeat(t(0), 1, null), 0, null)),
-            t(0).zeroOrMore()
+            simplify(parseRegex("x+*")),
+            parseRegex("x*")
     )
 
     @Test
     fun `nested constant neutralize`() = Assert.assertEquals(
-            simplify(RegularExpression.Repeat(RegularExpression.Repeat(t(0), 5, 5), 4, 4)),
-            RegularExpression.Repeat(t(0), 20, 20)
+            simplify(parseRegex("x{4}{5}")),
+            parseRegex("x{20}")
     )
 
     @Test
     fun `union extract prefix from concat`() = Assert.assertEquals(
-            simplify((t(0) concat t(1)) or (t(0) concat t(2))),
-            t(0) concat (t(1) or t(2))
+            simplify(parseRegex("x y | x z")),
+            parseRegex("x (y | z)")
     )
 
     @Test
     fun `union extract suffix from concat`() = Assert.assertEquals(
-            simplify((t(0) concat t(2)) or (t(1) concat t(2))),
-            (t(0) or t(1)) concat t(2)
+            simplify(parseRegex("x z | y z")),
+            parseRegex("(x | y) z")
     )
 
     @Test
     fun `union extract single prefix from twoOrMore`() = Assert.assertEquals(
-            simplify((t(1).repeat(2, null) concat t(2)) or (t(1) concat t(3))),
-            t(1) concat ((t(1).repeat(1, null) concat t(2)) or t(3))
+            simplify(parseRegex("x{2,} y | x z")),
+            parseRegex("x (x+ y | z)")
     )
 
     @Test
     fun `epsilon union to optional`() = Assert.assertEquals(
-            simplify(RegularExpression.empty<Int>() or t(1)),
-            t(1).repeat(0,1)
+            simplify(parseRegex("|x")),
+            parseRegex("x?")
     )
 
     //@Test todo: would be nice if this passed
