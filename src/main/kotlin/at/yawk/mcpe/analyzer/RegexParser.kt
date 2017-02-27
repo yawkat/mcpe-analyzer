861819ec53656112d15cdb69a63b3225e6a106a7
yawkat <me@yawk.at>
2017-02-27 11:49:58 +0100

    Regex parser

---
+++ src/main/kotlin/at/yawk/mcpe/analyzer/RegexParser.kt
@@ -0,0 +1,116 @@
+package at.yawk.mcpe.analyzer
+
+import java.util.ArrayList
+import java.util.HashSet
+import java.util.regex.Pattern
+
+/**
+ * @author yawkat
+ */
+fun parseRegex(input: String, terminalPattern: Pattern = Pattern.compile("\\w+")) = object : RegexParser<String>(input) {
+    val matcher = terminalPattern.matcher(input)
+
+    override fun takeTerminal(): RegularExpression.Terminal<String>? {
+        if (matcher.find(i) && matcher.start() == i) {
+            i = matcher.end()
+            return RegularExpression.Terminal(matcher.group())
+        } else {
+            return null
+        }
+    }
+}.parse()
+
+class ParseException(msg: String) : Exception(msg)
+
+private abstract class RegexParser<out T>(val input: String) {
+    var i = 0
+
+    fun hasRemaining() = i < input.length
+    fun peek() = input[i]
+    fun take() = input[i++]
+
+    inline fun take(predicate: (Char) -> Boolean): String {
+        val start = i
+        while (hasRemaining() && predicate(peek())) take()
+        return input.substring(start, i)
+    }
+
+    private fun skipWhitespace() {
+        while (hasRemaining() && peek().isWhitespace()) take()
+    }
+
+    abstract fun takeTerminal(): RegularExpression.Terminal<T>?
+
+    private fun unexpectedToken(token: String): Nothing = throw ParseException("Unexpected token '$token' near index $i")
+
+    fun parse(inParentheses: Boolean = false): RegularExpression<T> {
+        val or = ArrayList<List<RegularExpression<T>>>()
+        var concat = ArrayList<RegularExpression<T>>()
+        or.add(concat)
+
+        outer@ while (true) {
+            skipWhitespace()
+            if (!hasRemaining()) {
+                break
+            }
+            val terminal = takeTerminal()
+            if (terminal != null) {
+                concat.add(terminal)
+                continue
+            }
+            when (take()) {
+                '(' -> concat.add(parse(inParentheses = true))
+                ')' ->
+                    if (inParentheses) break@outer
+                    else unexpectedToken(")")
+                '|' -> {
+                    concat = ArrayList()
+                    or.add(concat)
+                }
+                '?' -> {
+                    if (concat.isEmpty()) unexpectedToken("?")
+                    concat[concat.size - 1] = concat.last().repeat(0, 1)
+                }
+                '*' -> {
+                    if (concat.isEmpty()) unexpectedToken("*")
+                    concat[concat.size - 1] = concat.last().repeat(0, null)
+                }
+                '+' -> {
+                    if (concat.isEmpty()) unexpectedToken("+")
+                    concat[concat.size - 1] = concat.last().repeat(1, null)
+                }
+                '{' -> {
+                    if (concat.isEmpty()) unexpectedToken("{")
+                    skipWhitespace()
+                    val min = take(Char::isDigit).toInt()
+                    skipWhitespace()
+                    if (take() != ',') throw ParseException("Expected ',' near index $i")
+                    skipWhitespace()
+                    val maxStr = take(Char::isDigit)
+                    val max = if (maxStr.isEmpty()) null else maxStr.toInt()
+                    skipWhitespace()
+                    if (take() != '}') throw ParseException("Expected ',' near index $i")
+                    concat[concat.size - 1] = concat.last().repeat(min, max)
+                }
+                'ε' -> concat.add(RegularExpression.empty())
+                '∅' -> concat.add(RegularExpression.nothing())
+                else -> throw ParseException("Unexpected token near index $i")
+            }
+        }
+        if (or.size == 1) {
+            if (concat.size == 1 && !inParentheses) {
+                return concat[0]
+            } else {
+                return RegularExpression.Concatenate(concat)
+            }
+        } else {
+            return RegularExpression.Or(or.mapTo(HashSet()) {
+                if (it.size == 1) {
+                    it[0]
+                } else {
+                    RegularExpression.Concatenate(it)
+                }
+            })
+        }
+    }
+}
---
+++ src/test/kotlin/at/yawk/mcpe/analyzer/RegexParserTest.kt
@@ -0,0 +1,93 @@
+package at.yawk.mcpe.analyzer
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+/**
+ * @author yawkat
+ */
+class RegexParserTest {
+    @Test
+    fun `terminal`() = Assert.assertEquals(
+            parseRegex("x"),
+            RegularExpression.Terminal("x")
+    )
+
+    @Test
+    fun `explicit concat`() = Assert.assertEquals(
+            parseRegex("(x)"),
+            RegularExpression.Concatenate(listOf(RegularExpression.Terminal("x")))
+    )
+
+    @Test
+    fun `terminal concat`() = Assert.assertEquals(
+            parseRegex("x y"),
+            RegularExpression.Terminal("x") concat RegularExpression.Terminal("y")
+    )
+
+    @Test
+    fun `terminal or`() = Assert.assertEquals(
+            parseRegex("x | y"),
+            RegularExpression.Terminal("x") or RegularExpression.Terminal("y")
+    )
+
+    @Test
+    fun `epsilon`() = Assert.assertEquals(
+            parseRegex("ε"),
+            RegularExpression.empty<String>()
+    )
+
+    @Test
+    fun `epsilon terminal`() = Assert.assertEquals(
+            parseRegex("ε x"),
+            RegularExpression.Concatenate(listOf(RegularExpression.empty<String>(), RegularExpression.Terminal("x")))
+    )
+
+    @Test
+    fun `nothing`() = Assert.assertEquals(
+            parseRegex("∅"),
+            RegularExpression.nothing<String>()
+    )
+
+    @Test
+    fun `or concat no parentheses`() = Assert.assertEquals(
+            parseRegex("x y | y"),
+            (RegularExpression.Terminal("x") concat RegularExpression.Terminal("y")) or RegularExpression.Terminal("y")
+    )
+
+    @Test
+    fun `or concat parentheses`() = Assert.assertEquals(
+            parseRegex("(x y) | y"),
+            (RegularExpression.Terminal("x") concat RegularExpression.Terminal("y")) or RegularExpression.Terminal("y")
+    )
+
+    @Test
+    fun `zero or more`() = Assert.assertEquals(
+            parseRegex("x*"),
+            RegularExpression.Terminal("x").repeat(0, null)
+    )
+
+    @Test
+    fun `one or more`() = Assert.assertEquals(
+            parseRegex("x+"),
+            RegularExpression.Terminal("x").repeat(1, null)
+    )
+
+    @Test
+    fun `zero or one`() = Assert.assertEquals(
+            parseRegex("x?"),
+            RegularExpression.Terminal("x").repeat(0, 1)
+    )
+
+    @Test
+    fun `lower bound`() = Assert.assertEquals(
+            parseRegex("x{2,}"),
+            RegularExpression.Terminal("x").repeat(2, null)
+    )
+
+    @Test
+    fun `double bound`() = Assert.assertEquals(
+            parseRegex("x{2,5}"),
+            RegularExpression.Terminal("x").repeat(2, 5)
+    )
+}
